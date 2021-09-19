45daea2a54c755f61e2a5699d116d5b915690ac5
codefromrvk <varshithkumar553@gmail.com>
2021-09-19 13:01:06 +0530

    update style

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const currentPrice = document.querySelector("#current-price")
 const btn = document.querySelector("#btn")
 const output = document.querySelector("#output")
 const outputImg = document.querySelector(".container2")
+const sub = document.querySelector("#sub")
 
 //costprice,number of stocks,selling price
 function calculateProfitAndLoss(cp, num, sp) {
@@ -29,26 +30,34 @@ function calculateProfitAndLoss(cp, num, sp) {
 }
 function displayOutput(str, result) {
     outputImg.classList.remove("invalid", "profit", "loss", "safe")
+
     if (result === "emptyornegative") {
-        output.style.backgroundColor = "gray";
+        output.style.backgroundColor = "brown";
         outputImg.classList.add("invalid");
         output.innerText = (str);
+        sub.innerText = "Kehna kya chahte ho?"
+        sub.style.backgroundColor = "brown";
 
     } else if (result === "profit") {
         output.style.backgroundColor = "green";
         outputImg.classList.add("profit");
         output.innerText = (str);
-
+        sub.innerText = "Paisa hii paisa!!!";
+        sub.style.backgroundColor = "green";
 
     } else if (result === "loss") {
         output.style.backgroundColor = "red";
         outputImg.classList.add("loss");
         output.innerText = (str);
+        sub.innerText = "Paisa sab barbaad!!!";
+        sub.style.backgroundColor = "red";
     }
     else {
         output.style.backgroundColor = "brown";
         outputImg.classList.add("safe");
-        output.innerText = (str);
+        output.innerText = 0.00;
+        sub.innerText = "Kuch bhi nahi ukaada";
+        sub.style.backgroundColor = "brown";
     }
 }
 
@@ -58,13 +67,13 @@ function clickHandler() {
 
     let outputText = "Kuch bhi nahi ukaada";
     if (ans === "emptyornegative") {
-        outputText = "Kehna Kya chahte ho?";
+        outputText = "Invalid Input";
         displayOutput(outputText, ans)
     } else if (ans === "profit") {
         outputText = `Profit:+Rs.${value} ( ${percentage.toFixed(2)}%)`;
         displayOutput(outputText, ans)
     } else if (ans === "loss") {
-        outputText = `Loss-Rs.${value} (${percentage.toFixed(2)}%) `
+        outputText = `Loss:-Rs.${value} (${percentage.toFixed(2)}%) `
         displayOutput(outputText, ans)
     } else {
         displayOutput(outputText, ans);
